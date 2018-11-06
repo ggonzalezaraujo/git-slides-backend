495511ad94c4ebacd2d8e9082ac13a5e0ab0bb1b
Giovanni Gonzalez <ggonzalezaraujo@ucmerced.edu>
2018-11-06 13:01:39 -0800

    finished queries

--- queries.sql
+++ queries.sql
@@ -5,21 +5,21 @@ FROM `User`;
 -- 2. Select a specific user
 SELECT *
 FROM `User`
-WHERE `email` = em;
+WHERE `email` = input_email;
 
 -- 3. Insert a new user
 INSERT INTO `User`
 (`id`, `first_name`, `last_name`, `email`, `password`, `type-fk`)
-VALUES (next_id, fname, lname, em, pw, type);
+VALUES (input_user_id, input_first_name, input_last_name, input_email, input_password, input_type);
 
 -- 4. Update user details
 UPDATE `User`
-SET `first_name` = fname, `last_name` = lname, `email` = em, `password` = pw
-WHERE `id` = input_id;
+SET `first_name` = input_first_name, `last_name` = input_last_name, `email` = input_email, `password` = input_password
+WHERE `id` = input_user_id;
 
 -- 5. Delete a user
 DELETE FROM `User`
-WHERE `id` = input_id;
+WHERE `id` = input_user_id;
 
 -- 6. List all courses
 SELECT *
@@ -40,28 +40,25 @@ AND u.`id` = r.`user-fk`;
 -- 9. Register user for a course
 INSERT INTO `Registration`
 (`id`, `user-fk`, `course-fk`)
-VALUES (next_id, input_user-fk, input_course-fk);
+VALUES (input_registration_id, input_user_id, input_course_id);
 
 -- 10. Remove user from a course
--- DELETE FROM Registration
--- WHERE user-fk=input_user-fk
--- AND course-fk=input_course-fk
 DELETE FROM `Registration`
-WHERE `id` = input_id;
+WHERE `id` = input_user_id;
 
 -- 11. Create a new course
 INSERT INTO `Course`
 (`id`, `title`, `semester`, `code`, `year`)
-VALUES (next_id, input_title, input_semester, input_code, input_year);
+VALUES (input_course_id, input_title, input_semester, input_code, input_year);
 
 -- 12. Delete an existing course
 DELETE FROM `Course`
-WHERE `id` = input_id;
+WHERE `id` = input_course_id;
 
 -- 13. Update a course
 UPDATE `Course`
 SET `title` = input_title, `semester` = input_semester, `code` = input_code, `year` = input_year
-WHERE `id` = input_id;
+WHERE `id` = input_course_id;
 
 -- 14. List all modules
 SELECT *
@@ -70,105 +67,75 @@ FROM `Module`;
 -- 15. List of modules for a given course
 SELECT m.`title`
 FROM `Course` c, `Module` m
-WHERE m.`course-fk` = c.`id` AND c.`id` = input_id;
+WHERE m.`course-fk` = c.`id` AND c.`id` = input_course_id;
 
 -- 16. List of modules per class for a given user
 SELECT c.`title`, m.`title`
 FROM `User` u, `Registration` r, `Course` c, `Module` m
-WHERE m.`course-fk` = c.`id` AND u.`id` = r.`user-fk` AND r.`course-fk` = c.`id` AND u.`id` = input_id;
+WHERE m.`course-fk` = c.`id` AND u.`id` = r.`user-fk` AND r.`course-fk` = c.`id` AND u.`id` = input_user_id;
 
 -- 17. Add a module for a course
 INSERT INTO `Module`
 (`id`, `title`, `course-fk`)
-VALUES (next_id, input_title, input_course-fk);
+VALUES (input_module_id, input_title, input_course_fk);
 
 -- 18. Delete a module for a given course
 DELETE FROM `Module`
-WHERE `id` = input_id;
+WHERE `id` = input_module_id;
 
 -- 19. Update a module for a given course
 UPDATE `Module`
 SET `title` = input_title
-WHERE `id` = input_id;
+WHERE `id` = input_module_id;
 
 -- 20. Get all the courses a student is registered for,
 SELECT *
-FROM Course c, Registration r, User u
-WHERE c.id = r.course-fk
-AND r.user-fk = u.id
+FROM `Course` c, `Registration` r, `User` u
+WHERE c.`id` = r.`course-fk`
+AND r.`user-fk`	 = u.`id`;
 
 -- 21. Student makes a submission
-INSERT INTO 'Submission'
-('id', 'user-fk', 'exercise-fk', 'grade')
-VALUES (next_id, input_user, input_exercise, input_grade)
+INSERT INTO `Submission`
+(`id`, `user-fk`, `exercise-fk`, `grade`)
+VALUES (input_submission_id, input_user_id, input_exercise, input_grade);
 
 -- 22. Get all submissions of a given student
 SELECT *
-FROM Submissions
-WHERE user-fk = input_student
+FROM `Submission`
+WHERE `user-fk` = input_user_id;
 
 -- 23. Specific student grade for a given module.. UPDATE THE SCHEMA TO INCLUDE A TOTAL VALUE OF POSSIBLE POINTS IN EXERCISE
-With SQ1 As (
-  SELECT exercise-fk, MAX(grade) as points
-  FROM Submissions
-  WHERE user-fk = input_user
-  GROUP BY exercise-fk
-)
-SELECT SUM(points)/e.total
-FROM Module m, Exercise e, SQ1
-WHERE e.id = SQ1.exercise-fk
-AND e.module-fk = m.id
+SELECT SUM(`points`)/SUM(e.`total`)
+FROM `Exercise` e, `Module` m, (SELECT s.`exercise-fk`, MAX(`grade`) as points
+								FROM `Submission` s, `Exercise` e, `Module` m
+								WHERE s.`exercise-fk` = e.`id` AND e.`module-fk` = m.`id` AND s.`user-fk` = input_user_id AND m.`id` = input_module_id
+								GROUP BY `exercise-fk`) AS SQ1
+WHERE e.`id` = SQ1.`exercise-fk` AND e.`module-fk` = m.`id`;
 
 -- 24. All students' grade for a given module.
-With SQ1 As (
-  SELECT user-fk, exercise-fk, MAX(grade) as points
-  FROM Submissions
-  GROUP BY user-fk, exercise-fk
-)
-SELECT SQ1.user-fk, SUM(points)/e.total
-FROM Module m, Exercise e, SQ1
-WHERE e.id = SQ1.exercise-fk
-AND e.module-fk = m.id
-GROUP BY SQ1.user-fk
+SELECT SQ1.`user-fk`, SUM(`points`)/SUM(e.`total`)
+FROM `Module` m, `Exercise` e, (SELECT `user-fk`, `exercise-fk`, MAX(`grade`) as points
+  								FROM `Submission` s, `Exercise` e, `Module` m
+  								WHERE s.`exercise-fk` = e.`id` AND e.`module-fk` = m.`id` AND m.`id` = input_module_id
+  								GROUP BY `user-fk`, `exercise-fk`) AS SQ1
+WHERE e.`id` = SQ1.`exercise-fk` AND e.`module-fk` = m.`id`
+GROUP BY SQ1.`user-fk`;
 
---25. Student grade for a given course
-With SQ2 As (
-  With SQ1 As (
-    SELECT user-fk, exercise-fk, MAX(grade) as points
-    FROM Submissions
-    WHERE user-fk = input_user
-    GROUP BY exercise-fk
-  )
-  SELECT SQ1.user-fk, m.id as module-fk, SUM(points)/e.total as grade
-  FROM Module m, Exercise e, SQ1
-  WHERE e.id = SQ1.exercise-fk
-  AND e.module-fk = m.id
-  GROUP BY m.id
-)
-SELECT SQ2.user-fk, m.id, SUM(SQ2.grade)
-FROM Course c, Module m, SQ2
-WHERE c.id = m.course-fk
-AND SQ2.module-fk = m.id
-AND c.id = input_course
-GROUP BY m.id
+-- 25. Student grade for a given course
+SELECT SUM(`points`)/SUM(e.`total`)
+FROM `Exercise` e, `Module` m, (SELECT s.`exercise-fk`, MAX(`grade`) as points
+								FROM `Submission` s, `Exercise` e, `Module` m, `Course` c
+								WHERE s.`exercise-fk` = e.`id` AND e.`module-fk` = m.`id` AND m.`course-fk` = c.`id` 
+								AND s.`user-fk` = input_user_id AND c.`id` = input_course_id
+								GROUP BY `exercise-fk`) AS SQ1
+WHERE e.`id` = SQ1.`exercise-fk` AND e.`module-fk` = m.`id`;
 
---26. Student grade for all students for a given course - ???
--- With SQ2 As (
---   With SQ1 As (
---     SELECT user-fk, exercise-fk, MAX(grade) as points
---     FROM Submissions
---     WHERE user-fk = input_user
---     GROUP BY exercise-fk
---   )
---   SELECT SQ1.user-fk, m.id as module-fk, SUM(points)/e.total as grade
---   FROM Module m, Exercise e, SQ1
---   WHERE e.id = SQ1.exercise-fk
---   AND e.module-fk = m.id
---   GROUP BY m.id
--- )
--- SELECT SQ2.user-fk, m.id, SUM(SQ2.grade)
--- FROM Course c, Module m, SQ2
--- WHERE c.id = m.course-fk
--- AND SQ2.module-fk = m.id
--- AND c.id = input_course
--- GROUP BY m.id
+-- 26. Student grade for all students for a given course
+SELECT SQ1.`user-fk`, SUM(`points`)/SUM(e.`total`)
+FROM `Exercise` e, `Module` m, (SELECT s.`user-fk`, s.`exercise-fk`, MAX(`grade`) as points
+								FROM `Submission` s, `Exercise` e, `Module` m, `Course` c
+								WHERE s.`exercise-fk` = e.`id` AND e.`module-fk` = m.`id` AND m.`course-fk` = c.`id` 
+								AND c.`id` = input_course_id
+								GROUP BY s.`user-fk`, s.`exercise-fk`) AS SQ1
+WHERE e.`id` = SQ1.`exercise-fk` AND e.`module-fk` = m.`id`
+GROUP BY SQ1.`user-fk`;
