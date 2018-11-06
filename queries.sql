c12d1d55947acfae5897cd3c120740f57c66756a
Mighty Chen <mchen73@ucmerced.edu>
2018-11-06 05:23:23 -0800

    included more queries

--- queries.sql
+++ queries.sql
@@ -72,7 +72,7 @@ SELECT m.`title`
 FROM `Course` c, `Module` m
 WHERE m.`course-fk` = c.`id` AND c.`id` = input_id;
 
--- 16. List of modules per class for a given user 
+-- 16. List of modules per class for a given user
 SELECT c.`title`, m.`title`
 FROM `User` u, `Registration` r, `Course` c, `Module` m
 WHERE m.`course-fk` = c.`id` AND u.`id` = r.`user-fk` AND r.`course-fk` = c.`id` AND u.`id` = input_id;
@@ -90,3 +90,85 @@ WHERE `id` = input_id;
 UPDATE `Module`
 SET `title` = input_title
 WHERE `id` = input_id;
+
+-- 20. Get all the courses a student is registered for,
+SELECT *
+FROM Course c, Registration r, User u
+WHERE c.id = r.course-fk
+AND r.user-fk = u.id
+
+-- 21. Student makes a submission
+INSERT INTO 'Submission'
+('id', 'user-fk', 'exercise-fk', 'grade')
+VALUES (next_id, input_user, input_exercise, input_grade)
+
+-- 22. Get all submissions of a given student
+SELECT *
+FROM Submissions
+WHERE user-fk = input_student
+
+-- 23. Specific student grade for a given module.. UPDATE THE SCHEMA TO INCLUDE A TOTAL VALUE OF POSSIBLE POINTS IN EXERCISE
+With SQ1 As (
+  SELECT exercise-fk, MAX(grade) as points
+  FROM Submissions
+  WHERE user-fk = input_user
+  GROUP BY exercise-fk
+)
+SELECT SUM(points)/e.total
+FROM Module m, Exercise e, SQ1
+WHERE e.id = SQ1.exercise-fk
+AND e.module-fk = m.id
+
+-- 24. All students' grade for a given module.
+With SQ1 As (
+  SELECT user-fk, exercise-fk, MAX(grade) as points
+  FROM Submissions
+  GROUP BY user-fk, exercise-fk
+)
+SELECT SQ1.user-fk, SUM(points)/e.total
+FROM Module m, Exercise e, SQ1
+WHERE e.id = SQ1.exercise-fk
+AND e.module-fk = m.id
+GROUP BY SQ1.user-fk
+
+--25. Student grade for a given course
+With SQ2 As (
+  With SQ1 As (
+    SELECT user-fk, exercise-fk, MAX(grade) as points
+    FROM Submissions
+    WHERE user-fk = input_user
+    GROUP BY exercise-fk
+  )
+  SELECT SQ1.user-fk, m.id as module-fk, SUM(points)/e.total as grade
+  FROM Module m, Exercise e, SQ1
+  WHERE e.id = SQ1.exercise-fk
+  AND e.module-fk = m.id
+  GROUP BY m.id
+)
+SELECT SQ2.user-fk, m.id, SUM(SQ2.grade)
+FROM Course c, Module m, SQ2
+WHERE c.id = m.course-fk
+AND SQ2.module-fk = m.id
+AND c.id = input_course
+GROUP BY m.id
+
+--26. Student grade for all students for a given course - ???
+-- With SQ2 As (
+--   With SQ1 As (
+--     SELECT user-fk, exercise-fk, MAX(grade) as points
+--     FROM Submissions
+--     WHERE user-fk = input_user
+--     GROUP BY exercise-fk
+--   )
+--   SELECT SQ1.user-fk, m.id as module-fk, SUM(points)/e.total as grade
+--   FROM Module m, Exercise e, SQ1
+--   WHERE e.id = SQ1.exercise-fk
+--   AND e.module-fk = m.id
+--   GROUP BY m.id
+-- )
+-- SELECT SQ2.user-fk, m.id, SUM(SQ2.grade)
+-- FROM Course c, Module m, SQ2
+-- WHERE c.id = m.course-fk
+-- AND SQ2.module-fk = m.id
+-- AND c.id = input_course
+-- GROUP BY m.id
